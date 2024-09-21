5b8933a7b910e1775e313169709b803ac00c3468
Dev-Garg-1 <gargdev010300@gmail.com>
2024-09-21 12:26:38 +0530

    updated main.js file

--- static/main.js
+++ static/main.js
@@ -27,29 +27,4 @@ if (TOKEN) {
     hide('.content.unauthorized');
     show('.content.authorized');
     checkFollowing(TOKEN);
-} else {
-    // Handle the OAuth flow by checking the code
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get('code');
-    
-    if (code) {
-        // Fetch the token from your backend
-        fetch(`/oauth-callback?code=${code}`)
-            .then(response => {
-                if (!response.ok) throw new Error('Network response was not ok');
-                return response.json();
-            })
-            .then(data => {
-                if (data.token) {
-                    hide('.content.unauthorized');
-                    show('.content.authorized');
-                    checkFollowing(data.token);
-                } else {
-                    console.error('No token received');
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
 }
